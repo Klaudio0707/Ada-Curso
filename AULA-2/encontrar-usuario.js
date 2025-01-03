b0b187739a24bba579d0b4b875b8464e78c692d4
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-01-02 22:01:15 -0300

    atualização aula02

--- AULA-2/encontrar-usuario.js
+++ AULA-2/encontrar-usuario.js
@@ -2,14 +2,19 @@ const listaDeContato = ["Paulo", "Selina", "Roger", "Rubens", "Zeca"];
 
 
 let encontrouUsuario = false;
+let atingiuFimDaLista = false;
 let indiceDoUsuario = 0;
 
-while (!encontrouUsuario) {
+while (!encontrouUsuario && !atingiuFimDaLista) {
     const usuarioAtual = listaDeContato[indiceDoUsuario];
-if (usuarioAtual.startsWith("P")){
+if (usuarioAtual.startsWith("m")){
     encontrouUsuario = true;
     console.log("Usuario encontrado " + usuarioAtual);
 }
 indiceDoUsuario = indiceDoUsuario + 1;
 
+if(indiceDoUsuario === listaDeContato.length){
+atingiuFimDaLista = true;
+console.log('Usuario não encontrado')
+}
 }
