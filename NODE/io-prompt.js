47f3ac13bfa1ae882eea0584a3f3fb12e5cd054d
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-01-03 22:33:01 -0300

    aula node

---
+++ NODE/io-prompt.js
@@ -0,0 +1,40 @@
+
+const rL = require('readline')
+
+const prompt = rL.createInterface({
+input: process.stdin,
+output: process.stdout
+})
+
+const promptPromise = {
+    question: (pergunta) => new Promise((resolve, rejects) => {
+        try {
+
+            prompt.question((pergunta), (resposta) => resolve(resposta))
+        } catch (error){
+rejects(error)
+        }
+    }),
+    close: () => prompt.close()
+}
+
+// prompt.question("Qual o seu numero favorito?:  ",(resposta) => {
+// console.log(`O dobro do seu número favorito é: ${parseInt(resposta)*2}`)
+
+// prompt.question("Qual a sua cor favorita:  ",(resposta) => {
+//     console.log(`Sua cor favorita é: ${resposta}`)
+//     prompt.close()
+//     })
+// })
+
+async function askUser () {
+const numero = await promptPromise.question('Qual o seu numero favorito:')
+console.log(`O dobro do seu número favorito é: ${parseInt(numero)*2}`)
+
+const cor = await promptPromise.question("Qual a sua cor favorita?: ")
+console.log("sua cor favorita é "  + cor )
+promptPromise.close()
+}
+
+
+askUser();
