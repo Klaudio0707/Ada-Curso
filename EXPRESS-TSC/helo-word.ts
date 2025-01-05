170fbb605e36637377092845a504cb204cacee02
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-01-05 13:18:42 -0300

    aula express

--- EXPRESS-TSC/helo-word.ts
+++ EXPRESS-TSC/helo-word.ts
@@ -1,12 +1,35 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import {config} from 'dotenv'
+import path from 'path'
+import {readFileSync} from 'fs'
 
+config()
 const app = express();
-const port = 3000;
+app.use('/client', express.static( path.join(__dirname, 'public'),{}))
+const url =  process.env.API_BASE_URL ?? 'http://localhost'; 
+const port = process.env.PORT ?? 3000;
+const users = [
+{
+name: 'Cláudio',
+age: 29
+},
+{
+name: "Bruna Alves",
+age: 25
+}
 
-app.get('/', (req: Request, res: Response) => {
-  return res.send('Hello World');
+]
+
+app.get('/', (req, res) => {
+//     const homePagePath = path.join(__dirname, 'home.html')
+// const homePage = readFileSync(homePagePath)
+//     return res.status(200).send(homePage)
 });
+app.get('/api/users', (req, res) => {
+return res.send(users)
 
+
+})
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on ${url}:${port}`);
 });
